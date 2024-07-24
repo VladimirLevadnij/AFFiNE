12f9a7c1c44ae0bf04f414d5f67e4516b638bb3a
Cats Juice <wp1298554944@hotmail.com>
2024-07-24 11:48:18 +0800

    feat(core): use new css variables

--- packages/frontend/core/src/components/app-sidebar/add-page-button/index.css.ts
+++ packages/frontend/core/src/components/app-sidebar/add-page-button/index.css.ts
@@ -1,17 +1,16 @@
-import { cssVar } from '@toeverything/theme';
+import { cssVarV2 } from '@toeverything/theme/v2';
 import { style } from '@vanilla-extract/css';
 
-// TODO(@CatsJuice): new variables are ready in design system
 export const root = style({
   width: 32,
   height: 32,
   borderRadius: 8,
   boxShadow: '0px 1px 2px 0px rgba(0, 0, 0, 0.15)',
-  border: `1px solid ${cssVar('borderColor')}`,
-  background: cssVar('white30'),
+  border: `1px solid ${cssVarV2('layer/border')}`,
+  background: cssVarV2('button/siderbarPrimary/background'),
 });
 export const icon = style({
-  color: cssVar('iconColor'),
+  color: cssVarV2('icon/primary'),
   fontSize: 20,
   display: 'block',
 });
